88766d9e33df0cf8ac750b95b2dc320b956c9bc0
Renzo Arteaga <u202214192@upc.edu.pe>
2026-05-06 22:52:38 -0500

    chore: initialize spring boot backend project

---
+++ widegreen_backend_project/src/main/java/pe/edu/upc/widegreen_backend_project/WidegreenBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.widegreen_backend_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidegreenBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidegreenBackendProjectApplication.class, args);
+	}
+
+}
---
+++ widegreen_backend_project/src/test/java/pe/edu/upc/widegreen_backend_project/WidegreenBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.widegreen_backend_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidegreenBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
